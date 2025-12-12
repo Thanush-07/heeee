93455953e2758b7ee273ca620b8c2ef7568588d7
Thanush-07 <thanushkumar722@gmail.com>
2025-12-12 20:04:50 +0530

    super --> institiona admin

---
+++ frontend/src/pages/Institution_admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Super Admin Dashboard</h1>
+      <p>Welcome to the company admin dashboard. Here you can manage your company's settings and view important metrics.</p>
+    </div>
+  );
+}
+export default Dashboard;
